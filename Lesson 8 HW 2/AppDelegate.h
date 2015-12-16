411b7060dd225ff26483967cfe98eabd3fc68b58
AlexeyUr <alisvcom@gmail.com>
2015-12-16 15:02:16 +0300

    Initial Commit

---
+++ Lesson 8 HW 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson 8 HW 2
+//
+//  Created by Alex on 16.12.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson 8 HW 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson 8 HW 2
+//
+//  Created by Alex on 16.12.15.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
